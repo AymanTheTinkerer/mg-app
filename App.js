204d2dab22b9c443a8a1becbadf434c0ec1e2ea1
AymanTheTinkerer <aymanismail2001@gmail.com>
2021-06-23 20:31:42 +0400

    "initialized mg app"

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
+// import 'react-native-gesture-handler';
+// import React from 'react';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import HomeScreen from './app/src/view/screens/HomeScreen';
+// import {Image} from 'react-native';
+// import Icon from 'react-native-vector-icons/MaterialIcons';
+// import CourseScreen from './app/src/view/screens/CourseScreen';
+// import {TouchableOpacity} from 'react-native-gesture-handler';
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         screenOptions={{ title: null, headerStyle: { elevation: 0 } }}
+//       >
+//         <Stack.Screen
+//           name="HomeScreen"
+//           component={HomeScreen}
+//           options={{
+//             headerLeft: () => (
+//               <Icon name="sort" size={25} style={{ marginLeft: 13 }} />
+//             ),
+//             headerRight: () => (
+//               <Image
+//                 style={{
+//                   height: 40,
+//                   width: 40,
+//                   marginRight: 20,
+//                   marginBottom: 8,
+//                 }}
+//                 source={require("./app/src/assets/person.png")}
+//               />
+//             ),
+//           }}
+//         ></Stack.Screen>
+//         <Stack.Screen
+//           name="CourseScreen"
+//           component="CourseScreen"
+//           options={({navigation}) => ({
+//             headerTransparent: true,
+//             headerLeft: () => (
+//               <TouchableOpacity
+//                 activeOpacity={0.8}
+//                 onPress={() => navigation.goBack()}>
+//                 <Icon
+//                   name="arrow-back-ios"
+//                   size={25}
+//                   style={{marginLeft: 20}}
+//                 />
+//               </TouchableOpacity>
+//             ),
+//             headerRight: () => (
+//               <Icon name="more-vert" size={25} style={{marginRight: 20}} />
+//             ),
+//           })}
+//         ></Stack.Screen>
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './app/src/view/screens/HomeScreen';
+import {Image} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import CourseScreen from './app/src/view/screens/CourseScreen';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
-export default function App() {
+const Stack = createStackNavigator();
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{title: null, headerStyle: {elevation: 0}}}>
+        <Stack.Screen
+          name="HomeSceen"
+          component={HomeScreen}
+          options={{
+            headerLeft: () => (
+              <Icon name="sort" size={25} style={{marginLeft: 20}} />
+            ),
+            headerRight: () => (
+              <Image
+                style={{height: 40, width: 40, marginRight: 20}}
+                source={require('./app/src/assets/person.png')}
+              />
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="CourseScreen"
+          options={({navigation}) => ({
+            headerTransparent: true,
+            headerLeft: () => (
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => navigation.goBack()}>
+                <Icon
+                  name="arrow-back-ios"
+                  size={25}
+                  style={{marginLeft: 20}}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <Icon name="more-vert" size={25} style={{marginRight: 20}} />
+            ),
+          })}
+          component={CourseScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
